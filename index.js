861fa3f8620a183df3ca014aab0d479fb4332d9f
GamerJonaton <76677694+flyare1337@users.noreply.github.com>
2022-04-15 16:32:09 +0300

    white theme

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 Vue.createApp({
     data() {
         return {
-            main: '<img src="logo.jpg" alt="" class="greeting-emoji"><div class="greeting-text" id="greeting-text">Hi, I am <b class="greeting-b">flyarrree</b></div>'
+            main: '<img src="logo.jpg" alt="" class="greeting-emoji"><div class="greeting-text" id="greeting-text">Hi, I am <b class="greeting-b">flyarrree</b></div>',
+            theme: "black",
+            themeIcon: '<i class="fa-solid fa-sun"></i>'
         }
     },
     methods: {
@@ -10,31 +12,23 @@ Vue.createApp({
             if (greeting.innerText == "Hi, I am flyarrree") {
                 this.main = '<img class="vay-ogo" src="https://sqdsh.top/static/wow.gif">'
             }
-    } 
+    }, setTheme() {
+      if(this.theme == 'black') {
+        document.documentElement.style.setProperty("--background-color", "#CECECE")
+        document.documentElement.style.setProperty("--text-color", "#2f3136")
+        document.documentElement.style.setProperty("--server-color", "#A7A7A7")
+        this.theme = "white"
+        this.themeIcon = '<i class="fa-solid fa-moon-stars"></i>' 
+      } else {
+        document.documentElement.style.setProperty("--background-color", "linear-gradient(to right, #36393F, #2F3136)")
+        document.documentElement.style.setProperty("--text-color", "rgb(212, 212, 212)")
+        document.documentElement.style.setProperty("--server-color", "#25272b")
+        this.theme = "black"
+        this.themeIcon = '<i class="fa-solid fa-sun"></i>'
+      }
+
+      }  
     }
 
 }).mount("#vue-app")
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    const followCursor = () => { 
-      const el = document.querySelector('.follow-cursor') 
-  
-      window.addEventListener('mousemove', e => { 
-        const target = e.target 
-        if (!target) return
-  
-        if (target.closest('a')) { 
-          el.classList.add('follow-cursor_active') 
-        } else { // иначе
-          el.classList.remove('follow-cursor_active') 
-        }
-  
-        el.style.left = e.pageX + 'px' 
-        el.style.top = e.pageY + 'px' 
-      })
-    }
-  
-    followCursor() 
-  
-  })
